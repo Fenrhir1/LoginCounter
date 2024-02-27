5ea557fa485ee4fd0867cce592045125e5cdaec7
Salvatore Polizzotti <fenrhir@libero.it>
2024-02-27 16:36:11 +0100

    fixed loggedPage js

--- src/components/loggedPage.ts
+++ src/components/loggedPage.ts
@@ -5,6 +5,7 @@ import { User } from '../declarations';
 export const loggedPage = () => {
   const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
   const loggedPage = document.createElement('div');
+
   loggedPage.innerHTML = `
         <h1>Utenti loggati</h1>
         <ul>
